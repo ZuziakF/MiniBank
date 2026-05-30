2758daa252805a83400377358c524eaaef6b488d
Filip Zuziak <filipzuziak18@gmail.com>
2026-05-30 21:32:01 +0200

    MiniBank.cpp update

---
+++ Bank.cpp
@@ -0,0 +1,52 @@
+#include "Bank.h"
+#include <iostream>
+#include <stdexcept>
+
+
+//Generowanie numeru konta
+std::string Bank::GenerateAccountNumber()
+{
+	std::string NewNumber = BankPrefix + std::to_string(NextClientNumber);
+	NextClientNumber++;
+	return NewNumber;
+}
+
+
+//Tworzenie konta
+void Bank::CreateAccount(const std::string& FirstName, const std::string LastName, double InitialBalance)
+{
+	std::string AccountNumber = GenerateAccountNumber();
+	Account NewAccount(AccountNumber, FirstName, LastName, InitialBalance);
+	Accounts.insert({AccountNumber,NewAccount});
+	std::cout << "Utworzono Konto";
+
+}
+
+
+//Depozyt
+void Bank::DepositToAccount(const std::string& AccountNumber, double Amount)
+{
+	auto it = Accounts.find(AccountNumber);
+	if (it == Accounts.end()) throw std::invalid_argument("Nie znaleziono konta o numerze " + AccountNumber);
+	it->second.Deposit(Amount);
+}
+
+
+//Wyplata
+void Bank::WithdrawFromAccount(const std::string& AccountNumber, double Amount)
+{
+	auto it = Accounts.find(AccountNumber);
+	if (it == Accounts.end()) throw std::invalid_argument("Nie znaleziono konta o numerze " + AccountNumber);
+
+	it->second.Withdraw(Amount);
+}
+
+//Pokazywanie informacji o koncie
+void Bank::DisplayAccountInfo(const std::string AccountNumber) const
+{
+	auto it = Accounts.find(AccountNumber);
+	if (it == Accounts.end())  throw std::invalid_argument("Blad: Nie znaleziono konta o numerze " + AccountNumber);
+
+	it->second.DisplayInfo();
+	
+}
--- MiniBank.cpp
+++ MiniBank.cpp
@@ -1,11 +1,130 @@
-﻿// MiniBank.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <string>
+#include <limits> // Do czyszczenia bufora konsoli
+#include "Bank.h"
 
-#include <iostream>
+void ClearInput() {
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Bank MyBank;
+	bool Running = true;
+	int Choice;
+
+	while (Running) 
+	{
+		//Wybor opcji
+		std::cout << "\nWybierz operacje:\n";
+		std::cout << "1. Utworz nowe konto\n";
+		std::cout << "2. Wplac srodki\n";
+		std::cout << "3. Wyplac srodki\n";
+		std::cout << "4. Wyswietl informacje o koncie\n";
+		std::cout << "5. Wyjscie\n";
+		std::cout << "Twoj wybor: ";
+
+		if (!(std::cin >> Choice)) {
+			std::cout << "Nieprawidlowy wybor. \n";
+			ClearInput();
+			continue;
+		}
+		std::string AccNumber, FirstName, LastName;
+		double Amount;
+
+
+		switch (Choice) 
+		{
+		//Zakladanie nowego konta
+		case 1:
+			std::cout << "Podaj imie: ";
+			std::cin >> FirstName;
+			std::cout << "Podaj nazwisko ";
+			std::cin >> LastName;
+
+			try 
+			{
+				MyBank.CreateAccount(FirstName, LastName);
+			} 
+			catch (const std::exception& e)
+			{
+				std::cout << "Blad: " << e.what() << "\n";
+			}
+			break;
+
+
+		//Wplata
+		case 2:
+			std::cout << "Podaj numer konta: ";
+			std::cin >> AccNumber;
+			std::cout << "Podaj kwote do wplaty: ";
+			if (!(std::cin >> Amount))
+			{
+				std::cout << "Nieprawidlowa kwota.\n";
+				ClearInput();
+				break;
+			}
+			try
+			{
+				MyBank.DepositToAccount(AccNumber, Amount);
+				std::cout << "Wplata zakonczona!\n";
+			}
+			catch (const std::exception& e) 
+			{
+				std::cout << e.what() << "\n";
+			}
+
+
+		//Wyplata
+		case 3:
+			std::cout << "Podaj numer konta: ";
+			std::cin >> AccNumber;
+			std::cout << "Podaj kwote do wplaty: ";
+			if (!(std::cin >> Amount))
+			{
+				std::cout << "Nieprawidlowa kwota.\n";
+				ClearInput();
+				break;
+			}
+			try
+			{
+				MyBank.WithdrawFromAccount(AccNumber, Amount);
+				std::cout << "Wyplata zakonczona!\n";
+			}
+			catch (const std::exception& e)
+			{
+				std::cout << e.what() << "\n";
+			}
+
+
+		//Wyswietlanie informacji
+		case 4:
+			std::cout << "Podaj numer konta: ";
+			std::cin >> AccNumber;
+			try
+			{
+				MyBank.DisplayAccountInfo(AccNumber);
+			}
+			catch (const std::exception& e)
+			{
+				std::cout<< e.what() << "\n";
+			}
+			break;
+
+
+		//Wyjscie
+		case 5:
+			std::cout << "Dziekujemy za skrozystanie z banku. Do uslyszenia!";
+			Running = false;
+			break;
+		default:
+			std::cout << "Nieznana opcja. Wybierz cyfre od 1 do 5.";
+			break;
+		}
+	}
+	return 0;
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
